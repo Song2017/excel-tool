8b0c4cdc3c977fe65701e819cb0ce9dc9a6256d7
Ben Song <ben.song@samarkand.global>
2024-04-09 11:52:51 +0800

    add sheet and group output

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from src.db.db_query import execute_query
-
 import json
+from src.output.to_excel import export_excel, append_sheet_title, get_excel_range
 
 
 def loader() -> dict:
@@ -11,12 +11,41 @@ def loader() -> dict:
 
 def main():
     conf = loader()
+    file_name = "./dev.xlsx"
+    # sheet
     for k, v in conf.items():
-        # sheet
-        ...
-    import pdb;
-    pdb.set_trace()
-    rows = execute_query("", {})
+        if not k.startswith("sheet"):
+            continue
+        sheet_index = int(k.split()[1]) - 1
+        export_excel([""], [""], sheet_name=v.get('name'), file_path=file_name)
+        # period
+        period = v.get('period')
+        er = get_excel_range(period["base_line"], period["height"], period["column"])
+        append_sheet_title(er[0], er[1], value=period.get("name"),
+                           file_path=file_name, sheet_index=sheet_index)
+        # groups
+        for key in v.keys():
+            if not key.startswith("group"):
+                continue
+            gp = v.get(key)
+            gp_base_line = gp["base_line"]
+            er = get_excel_range(gp["base_line"], gp["height"], gp["column"])
+            append_sheet_title(er[0], er[1], value=gp.get("name"),
+                               file_path=file_name, sheet_index=sheet_index, font_size=16)
+            # table
+            for t_key in gp.keys():
+                if not t_key.startswith("table"):
+                    continue
+                t = gp.get(t_key)
+                er = get_excel_range(t["inc_line"]+gp_base_line, t["height"], t["column"])
+                append_sheet_title(er[0], er[1], value=t.get("name"),
+                                   file_path=file_name, sheet_index=sheet_index)
+                # head
+                head = t.get('head')
+
+
+
+    # rows = execute_query("", {})
 
 
 if __name__ == '__main__':
--- src/output/to_excel.py
+++ src/output/to_excel.py
@@ -1,52 +1,66 @@
 import os
 import pandas as pd
 from openpyxl import load_workbook
-from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Alignment
+from openpyxl.cell import Cell
+from openpyxl.styles import PatternFill, Alignment, Font
+from openpyxl.worksheet.worksheet import Worksheet
 
 
-def export_orders(
-        i_data: list,
-        i_columns: list,
-        path: str,
-        excel_name="all_orders.xlsx",
-        sheet_name="order") -> str:
+def get_excel_range(base: int, height: int, column: str) -> tuple:
+    cols = column.split()
+    return cols[0] + str(base), cols[1] + str(base + height - 1)
+
+
+def export_excel(i_data: list, i_columns: list,
+                 file_path: str = "./demo.xlsx", sheet_name="order") -> str:
     db = pd.DataFrame(i_data, columns=i_columns, dtype=str)
-    writer = pd.ExcelWriter(os.path.join(path, excel_name))
-    db.to_excel(writer, sheet_name=sheet_name)
-    worksheet = writer.sheets[sheet_name]
-    writer._save()
-    return excel_name
+    mode = "a" if os.path.exists(file_path) else "w"
+    ise = "replace" if mode == "a" else None
+    with pd.ExcelWriter(file_path, engine="openpyxl", mode=mode, if_sheet_exists=ise) as writer:
+        db.to_excel(writer, sheet_name=sheet_name, index=False)
+    return file_path
 
 
-def append_table(rows: list, sheet_index: int = 0, interval: int = 1, path='./', file_name: str = "all_orders.xlsx"):
-    excel_name = os.path.join(path, file_name)
+def append_table(rows: list, sheet_name: str = "", sheet_index: int = 0, interval: int = 1, file_path='./demo.xlsx'):
     # 加载已存在的工作簿
-    wb = load_workbook(excel_name)
+    wb = load_workbook(file_path)
     # 选择要追加数据的工作表，这里假设是第一个工作表
-    ws = wb._sheets[sheet_index]
+    if sheet_name:
+        ws = wb[sheet_name]
+    else:
+        ws = wb.worksheets[sheet_index]
     # 获取当前工作表中的最大行数
     max_row = ws.max_row
     max_row += interval
     # 追加一行数据，注意行号是从1开始的，所以新行的行号是 max_row + 1
     for col_num, value in enumerate(rows, start=1):
         cell = ws.cell(row=max_row + 1, column=col_num, value=value)
-
+        print(cell)
         # 保存工作簿
-    wb.save(excel_name)
+    wb.save(file_path)
 
 
-def append_sheet_title(start: str, end: str, value: str = "", sheet_index: int = 0, path='./',
-                       file_name: str = "all_orders.xlsx"):
+def append_table_head(ws: Worksheet, row: int, column: int, values: list, color="709bfa", font_size=11):
+    ws.row_dimensions[row].height = 80
+    for i, v in enumerate(values):
+        cell: Cell = ws.cell(row=row, column=column + i, value=v)
+        cell.font = Font(size=font_size)
+        cell.fill = PatternFill(start_color=color, end_color=color, fill_type='solid')
+
+
+def append_sheet_title(start: str, end: str, value: str = "", sheet_name: str = "", sheet_index: int = 0,
+                       file_path='./demo.xlsx', color="709bfa", font_size=11):
     # 创建一个新的工作簿
-    excel_name = os.path.join(path, file_name)
     # 加载已存在的工作簿
-    wb = load_workbook(excel_name)
+    wb = load_workbook(file_path)
     # 选择要追加数据的工作表，这里假设是第一个工作表
-    ws = wb._sheets[sheet_index]
+    if sheet_name:
+        ws = wb[sheet_name]
+    else:
+        ws = wb.worksheets[sheet_index]
 
     # 设置填充颜色为蓝色
-    fill = PatternFill(start_color='709bfa', end_color='709bfa', fill_type='solid')
+    fill = PatternFill(start_color=color, end_color=color, fill_type='solid')
     # 合并单元格，从 A1 到 C2（跨3列，高2行）
     ws.merge_cells(f'{start}:{end}')
     # 选择合并后的单元格（合并后仍然可以通过左上角的单元格引用）
@@ -55,14 +69,15 @@ def append_sheet_title(start: str, end: str, value: str = "", sheet_index: int =
     merged_cell.fill = fill
     # 设置文本内容
     merged_cell.value = value or '合并单元格文本'
+    merged_cell.font = Font(size=font_size)
     # 设置文本内容居中
     merged_cell.alignment = Alignment(horizontal='center', vertical='center')
     # 保存工作簿
-    wb.save(excel_name)
+    wb.save(file_path)
 
 
 if __name__ == '__main__':
-    export_orders([1, 2, 3], ['column'], "./")
-
+    export_excel([1, 2, 3], ['column'])
+    export_excel([1, 2, 3], ['column'], sheet_name="ss")
     # append_table([1, 2, 3])
-    append_sheet_title("A1", "D1")
+    append_sheet_title("A1", "D2")
